68489bd40cbd59df0ae2901236318974f317ce09
Telmo Trooper <telmo.trooper@gmail.com>
2018-10-07 18:50:50 -0300

    Added Loony Lips project

---
+++ gamedevtv/1-loony_lips/LoonyLips.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _ready():
+	var prompt = ["Yann", "pizza", "full"]
+	var story = "Once upon a time a %s ate a %s and felt very %s."
+	$Blackboard/StoryText.text = story % prompt
+	$Blackboard/TextBox.text = ""
+
+func _on_TextureButton_pressed():
+	var new_text = $Blackboard/TextBox.get_text()
+	_on_TextBox_text_entered(new_text)
+
+func _on_TextBox_text_entered(new_text):
+	$Blackboard/StoryText.text = new_text
+	$Blackboard/TextBox.text = ""
