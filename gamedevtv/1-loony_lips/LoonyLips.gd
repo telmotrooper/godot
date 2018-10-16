42ae84e37530f09174a442488d1e8d86468645d1
Telmo Trooper <telmo.trooper@gmail.com>
2018-10-16 19:58:41 -0300

    Loony Lips now has multiple stories

--- gamedevtv/1-loony_lips/LoonyLips.gd
+++ gamedevtv/1-loony_lips/LoonyLips.gd
@@ -1,41 +1,54 @@
 extends Node2D
 
 var player_words = []
-var prompt = ["a name", "a thing", "a feeling", "another feeling", "some things"]
-var story = "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s."
+var template = [
+  {
+    "prompt" : ["a name", "a thing", "a feeling", "another feeling", "some things"],
+    "story" : "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s."
+  },
+  {
+    "prompt" : ["an animal", "a food", "a feeling"],
+    "story" : "There was once a %s that really liked %s. One day he got so %s that he ate it all at once."
+  }
+]
+var current_story
 
 func _ready():
-	$Blackboard/StoryText.text = "Welcome to Loony Lips!\n\nWe're going to tell a story and have a lovely time!\nCan I have " + prompt[player_words.size()] + ", please?"
-	$Blackboard/TextBox.text = ""
+  randomize()
+  current_story = template[randi() % template.size()]
+  $Blackboard/StoryText.text = "Welcome to Loony Lips!" + "\n\n" \
+  + "We're going to tell a story and have a lovely time!" + "\n\n" \
+  + "Can I have " + current_story.prompt[player_words.size()] + ", please?"
+  $Blackboard/TextBox.text = ""
 
 func _on_TextureButton_pressed():
-	if is_story_done():
-		get_tree().reload_current_scene()
-	else:
-		var new_text = $Blackboard/TextBox.get_text()
-		_on_TextBox_text_entered(new_text)
+  if is_story_done():
+    get_tree().reload_current_scene()
+  else:
+    var new_text = $Blackboard/TextBox.get_text()
+    _on_TextBox_text_entered(new_text)
 
 func _on_TextBox_text_entered(new_text):
-	player_words.append(new_text)
-	$Blackboard/TextBox.text = ""
-	check_player_word_length()
+  player_words.append(new_text)
+  $Blackboard/TextBox.text = ""
+  check_player_word_length()
 
 func is_story_done():
-	return player_words.size() == prompt.size()
+  return player_words.size() == current_story.prompt.size()
 
 func prompt_player():
-	$Blackboard/StoryText.text = ("Can I have " + prompt[player_words.size()] + ", please?")
+  $Blackboard/StoryText.text = ("Can I have " + current_story.prompt[player_words.size()] + ", please?")
 
 func check_player_word_length():
-	if is_story_done():
-		tell_story()
-	else:
-		prompt_player()
+  if is_story_done():
+    tell_story()
+  else:
+    prompt_player()
 
 func tell_story():
-	$Blackboard/StoryText.text = story % player_words
-	end_game()
+  $Blackboard/StoryText.text = current_story.story % player_words
+  end_game()
 
 func end_game():
-	$Blackboard/TextBox.queue_free()
-	$Blackboard/TextureButton/RichTextLabel.text = "Again!"
+  $Blackboard/TextBox.queue_free()
+  $Blackboard/TextureButton/RichTextLabel.text = "Again!"
