31fb51f1d48e57cabfa7505a246c8fc415e724d3
Telmo Trooper <telmo.trooper@gmail.com>
2018-10-16 00:25:42 -0300

    Loony Lips now can be restarted

--- gamedevtv/1-loony_lips/LoonyLips.gd
+++ gamedevtv/1-loony_lips/LoonyLips.gd
@@ -9,22 +9,33 @@ func _ready():
 	$Blackboard/TextBox.text = ""
 
 func _on_TextureButton_pressed():
-	var new_text = $Blackboard/TextBox.get_text()
-	_on_TextBox_text_entered(new_text)
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		var new_text = $Blackboard/TextBox.get_text()
+		_on_TextBox_text_entered(new_text)
 
 func _on_TextBox_text_entered(new_text):
 	player_words.append(new_text)
 	$Blackboard/TextBox.text = ""
 	check_player_word_length()
 
+func is_story_done():
+	return player_words.size() == prompt.size()
+
 func prompt_player():
 	$Blackboard/StoryText.text = ("Can I have " + prompt[player_words.size()] + ", please?")
 
 func check_player_word_length():
-	if player_words.size() == prompt.size():
+	if is_story_done():
 		tell_story()
 	else:
 		prompt_player()
 
 func tell_story():
 	$Blackboard/StoryText.text = story % player_words
+	end_game()
+
+func end_game():
+	$Blackboard/TextBox.queue_free()
+	$Blackboard/TextureButton/RichTextLabel.text = "Again!"
