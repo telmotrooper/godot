9f9dfd43983e085b978f7df24a0b16264f7cc6e7
Telmo Trooper <telmo.trooper@gmail.com>
2018-12-27 00:02:01 -0200

    Basic gravity and jumping added

--- gamedevtv/2-hoppy_days/Scripts/Player.gd
+++ gamedevtv/2-hoppy_days/Scripts/Player.gd
@@ -1,10 +1,29 @@
 extends KinematicBody2D
 
 const SPEED = 750
+const GRAVITY = 2500
+const UP = Vector2(0, -1)
+const JUMP_SPEED = -1000
 
 var motion = Vector2()
 
 func _physics_process(delta):
+  fall(delta)
+  run()
+  jump()
+  move_and_slide(motion, UP)
+
+func fall(delta):
+  if is_on_floor():
+    motion.y = 0
+  else:
+    motion.y += GRAVITY * delta
+
+func jump():
+  if is_on_floor() and Input.is_action_pressed("ui_up"):
+    motion.y = JUMP_SPEED
+
+func run():
   if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
     motion.x = SPEED
     $AnimatedSprite.play("run")
@@ -19,8 +38,3 @@ func _physics_process(delta):
     motion.x = 0
     $AnimatedSprite.play("idle")
     $AnimatedSprite.flip_h = false
-  
-  move_and_slide(motion)
-
-func _ready():
-  pass
