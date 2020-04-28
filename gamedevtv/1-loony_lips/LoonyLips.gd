7968c2420f0d735b5c1bad954751208384666494
Telmo Silva <tvsilva@tech6.com.br>
2020-04-28 19:29:26 -0300

    Fixed spacing in old projects

--- gamedevtv/1-loony_lips/LoonyLips.gd
+++ gamedevtv/1-loony_lips/LoonyLips.gd
@@ -5,56 +5,56 @@ var strings # all the text that is displayed to the player
 var current_story
 
 func _ready():
-  set_random_story()
-  strings = get_from_json("other_strings.json")
-  
-  $Blackboard/StoryText.text = strings.intro_text
-  prompt_player()
-  $Blackboard/TextBox.text = ""
+	set_random_story()
+	strings = get_from_json("other_strings.json")
+	
+	$Blackboard/StoryText.text = strings.intro_text
+	prompt_player()
+	$Blackboard/TextBox.text = ""
 
 func set_random_story():
-  var stories = get_from_json("stories.json")
-  randomize()
-  current_story = stories[randi() % stories.size()]
+	var stories = get_from_json("stories.json")
+	randomize()
+	current_story = stories[randi() % stories.size()]
 
 func get_from_json(filename):
-  var file = File.new() # the file object
-  file.open(filename, File.READ) # assuming the file exists
-  var text = file.get_as_text()
-  var data = parse_json(text)
-  file.close()
-  return data
+	var file = File.new() # the file object
+	file.open(filename, File.READ) # assuming the file exists
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close()
+	return data
 
 func _on_TextureButton_pressed():
-  if is_story_done():
-    get_tree().reload_current_scene()
-  else:
-    var new_text = $Blackboard/TextBox.get_text()
-    _on_TextBox_text_entered(new_text)
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		var new_text = $Blackboard/TextBox.get_text()
+		_on_TextBox_text_entered(new_text)
 
 func _on_TextBox_text_entered(new_text):
-  player_words.append(new_text)
-  $Blackboard/TextBox.text = ""
-  $Blackboard/StoryText.text = ""
-  check_player_word_length()
+	player_words.append(new_text)
+	$Blackboard/TextBox.text = ""
+	$Blackboard/StoryText.text = ""
+	check_player_word_length()
 
 func is_story_done():
-  return player_words.size() == current_story.prompt.size()
+	return player_words.size() == current_story.prompt.size()
 
 func prompt_player():
-  var next_prompt = current_story.prompt[player_words.size()]
-  $Blackboard/StoryText.text += strings.prompt % next_prompt
+	var next_prompt = current_story.prompt[player_words.size()]
+	$Blackboard/StoryText.text += strings.prompt % next_prompt
 
 func check_player_word_length():
-  if is_story_done():
-    tell_story()
-  else:
-    prompt_player()
+	if is_story_done():
+		tell_story()
+	else:
+		prompt_player()
 
 func tell_story():
-  $Blackboard/StoryText.text = current_story.story % player_words
-  end_game()
+	$Blackboard/StoryText.text = current_story.story % player_words
+	end_game()
 
 func end_game():
-  $Blackboard/TextBox.queue_free()
-  $Blackboard/TextureButton/RichTextLabel.text = strings.again
+	$Blackboard/TextBox.queue_free()
+	$Blackboard/TextureButton/RichTextLabel.text = strings.again
--- gamedevtv/2-hoppy_days/Scripts/GameState.gd
+++ gamedevtv/2-hoppy_days/Scripts/GameState.gd
@@ -4,14 +4,15 @@ export var starting_lives = 3
 var lives
 
 func _ready():
-  Global.GameState = self
-  lives = starting_lives
+	Global.GameState = self
+	lives = starting_lives
 
 func hurt():
-  lives -= 1
-  Global.Player.hurt()
-  if lives < 0:
-    end_game()
+	lives -= 1
+	Global.Player.hurt()
+	
+	if lives < 0:
+		end_game()
 
 func end_game():
-  get_tree().change_scene("res://Scenes/GameOver.tscn")
+	get_tree().change_scene("res://Scenes/GameOver.tscn")
