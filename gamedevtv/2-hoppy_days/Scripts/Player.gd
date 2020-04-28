6c18547ce649b0cda080f4b367182482d5716769
Telmo Silva <tvsilva@tech6.com.br>
2020-04-28 20:01:31 -0300

    Added music and sound effects to Hoppy Days

--- gamedevtv/2-hoppy_days/Scripts/Player.gd
+++ gamedevtv/2-hoppy_days/Scripts/Player.gd
@@ -9,46 +9,46 @@ var motion = Vector2()
 export var world_limit = 3200
 
 func _ready():
-  Global.Player = self
+	Global.Player = self
 
 func _physics_process(delta):
-  fall(delta)
-  run()
-  jump()
-  move_and_slide(motion, UP)
+	fall(delta)
+	run()
+	jump()
+	move_and_slide(motion, UP)
 
 func fall(delta):
-  if is_on_floor():
-    motion.y = 0
-  elif is_on_ceiling():
-    motion.y = 1
-  else:
-    motion.y += GRAVITY * delta
+	if is_on_floor():
+		motion.y = 0
+	elif is_on_ceiling():
+		motion.y = 1
+	else:
+		motion.y += GRAVITY * delta
 
-  if position.y > world_limit:
-    Global.GameState.end_game()
+	if position.y > world_limit:
+		Global.GameState.end_game()
 
 func hurt():
-  motion.y = JUMP_SPEED
+	motion.y = JUMP_SPEED
+	$PainSFX.play()
 
 func jump():
-  if is_on_floor() and Input.is_action_pressed("ui_up"):
-    motion.y = JUMP_SPEED
+	if is_on_floor() and Input.is_action_pressed("ui_up"):
+		motion.y = JUMP_SPEED
+		$JumpSFX.play()
 
 func run():
-  if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
-    motion.x = SPEED
-    $AnimatedSprite.play("run")
-    $AnimatedSprite.flip_h = false
-    
-  elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
-    motion.x = -SPEED
-    $AnimatedSprite.play("run")
-    $AnimatedSprite.flip_h = true
-    
-  else:
-    motion.x = 0
-    $AnimatedSprite.play("idle")
-    $AnimatedSprite.flip_h = false
-
-
+	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+		motion.x = SPEED
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = false
+	
+	elif Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+		motion.x = -SPEED
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = true
+	
+	else:
+		motion.x = 0
+		$AnimatedSprite.play("idle")
+		$AnimatedSprite.flip_h = false
